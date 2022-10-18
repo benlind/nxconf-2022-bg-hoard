25de17b0d4edfcdd253ce184c9bbde43ef1825ce
Ben Lind <ben.lind@grantstreet.com>
2022-10-18 19:29:33 -0400

    lab22 fix deployment

--- libs/internal-plugin/src/executors/heroku-deploy/executor.ts
+++ libs/internal-plugin/src/executors/heroku-deploy/executor.ts
@@ -3,10 +3,11 @@ import { execSync } from 'child_process';
 
 export default async function runExecutor(schema: HerokuDeployExecutorSchema) {
   const cwd = schema.distLocation;
-  execSync(`heroku container:login`, { cwd });
-  execSync(`heroku container:push web --app ${schema.herokuAppName}`, { cwd });
+  execSync(`heroku container:login`, { cwd, stdio: 'inherit' });
+  execSync(`heroku container:push web --app ${schema.herokuAppName}`, { cwd, stdio: 'inherit' });
   execSync(`heroku container:release web --app ${schema.herokuAppName}`, {
     cwd,
+    stdio: 'inherit',
   });
   return {
     success: true,
