2265f89999907c0f7d629cb7a0b7fdb6480106bb
Ben Lind <ben.lind@grantstreet.com>
2022-10-18 14:30:13 -0400

    lab14

--- libs/internal-plugin/src/generators/update-scope-schema/generator.ts
+++ libs/internal-plugin/src/generators/update-scope-schema/generator.ts
@@ -1,12 +1,36 @@
-import { formatFiles, installPackagesTask, Tree, updateJson } from '@nrwl/devkit';
+import {
+  Tree,
+  updateJson,
+  formatFiles,
+  ProjectConfiguration,
+  getProjects,
+} from '@nrwl/devkit';
+
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const projects: any[] = Array.from(projectMap.values());
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags.filter((tag: string) => tag.startsWith('scope:'))
+    )
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    .map((scope: string) => scope.slice(6));
+  return Array.from(new Set(allScopes));
+}
 
 export default async function (tree: Tree) {
-  await updateJson(tree, 'nx.json', value => {
-    value.defaultProject = 'api'
-    return value
-  })
+  const scopes = getScopes(getProjects(tree));
+  updateJson(
+    tree,
+    'libs/internal-plugin/src/generators/util-lib/schema.json',
+    (schemaJson) => {
+      schemaJson.properties.directory['x-prompt'].items = scopes.map(
+        (scope) => ({
+          value: scope,
+          label: scope,
+        })
+      );
+      return schemaJson;
+    }
+  );
   await formatFiles(tree);
-  return () => {
-    installPackagesTask(tree);
-  };
 }
