a64387f4623c3a252e5bac01873e1677c9b6e585
Ben Lind <ben.lind@grantstreet.com>
2022-10-18 13:50:30 -0400

    Use solution

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -1,77 +1,96 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  getWorkspaceLayout,
-  names,
-  offsetFromRoot,
-  Tree,
-} from '@nrwl/devkit';
-import * as path from 'path';
+import { formatFiles, installPackagesTask, Tree } from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
 import { UtilLibGeneratorSchema } from './schema';
 
-interface NormalizedSchema extends UtilLibGeneratorSchema {
-  projectName: string;
-  projectRoot: string;
-  projectDirectory: string;
-  parsedTags: string[];
-}
-
-function normalizeOptions(tree: Tree, options: UtilLibGeneratorSchema): NormalizedSchema {
-  const name = names(options.name).fileName;
-  const projectDirectory = options.directory
-    ? `${names(options.directory).fileName}/${name}`
-    : name;
-  const projectName = 'util-' + projectDirectory.replace(new RegExp('/', 'g'), '-');
-  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
-  const parsedTags = options.tags
-    ? options.tags.split(',').map((s) => s.trim())
-    : [];
-  if (!parsedTags.includes('type:util')) {
-    parsedTags.push('type:util')
-  }
-  const directoryScope = `scope:${options.directory}`
-  if (!parsedTags.includes(directoryScope)) {
-    parsedTags.push(directoryScope)
-  }
-
-  return {
-    ...options,
-    projectName,
-    projectRoot,
-    projectDirectory,
-    parsedTags,
+export default async function (tree: Tree, schema: UtilLibGeneratorSchema) {
+  await libraryGenerator(tree, {
+    name: `util-${schema.name}`,
+    directory: schema.directory,
+    tags: `type:util, scope:${schema.directory}`,
+  });
+  await formatFiles(tree);
+  return () => {
+    installPackagesTask(tree);
   };
 }
 
-function addFiles(tree: Tree, options: NormalizedSchema) {
-    const templateOptions = {
-      ...options,
-      ...names(options.name),
-      offsetFromRoot: offsetFromRoot(options.projectRoot),
-      template: ''
-    };
-    generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
-}
+// import {
+//   addProjectConfiguration,
+//   formatFiles,
+//   generateFiles,
+//   getWorkspaceLayout,
+//   names,
+//   offsetFromRoot,
+//   Tree,
+// } from '@nrwl/devkit';
+// import * as path from 'path';
+// import { UtilLibGeneratorSchema } from './schema';
+// // import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (tree: Tree, options: UtilLibGeneratorSchema) {
-  const normalizedOptions = normalizeOptions(tree, options);
+// interface NormalizedSchema extends UtilLibGeneratorSchema {
+//   projectName: string;
+//   projectRoot: string;
+//   projectDirectory: string;
+//   parsedTags: string[];
+// }
 
-  addProjectConfiguration(
-    tree,
-    normalizedOptions.projectName,
-    {
-      root: normalizedOptions.projectRoot,
-      projectType: 'library',
-      sourceRoot: `${normalizedOptions.projectRoot}/src`,
-      targets: {
-        build: {
-          executor: "@bg-hoard/internal-plugin:build",
-        },
-      },
-      tags: normalizedOptions.parsedTags,
-    }
-  );
-  addFiles(tree, normalizedOptions);
-  await formatFiles(tree);
-}
+// function normalizeOptions(tree: Tree, options: UtilLibGeneratorSchema): NormalizedSchema {
+//   const name = names(options.name).fileName;
+//   const projectDirectory = options.directory
+//     ? `${names(options.directory).fileName}/${name}`
+//     : name;
+//   const projectName = 'util-' + projectDirectory.replace(new RegExp('/', 'g'), '-');
+//   const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
+//   const parsedTags = options.tags
+//     ? options.tags.split(',').map((s) => s.trim())
+//     : [];
+//   if (!parsedTags.includes('type:util')) {
+//     parsedTags.push('type:util')
+//   }
+//   const directoryScope = `scope:${options.directory}`
+//   if (!parsedTags.includes(directoryScope)) {
+//     parsedTags.push(directoryScope)
+//   }
+
+//   return {
+//     ...options,
+//     projectName,
+//     projectRoot,
+//     projectDirectory,
+//     parsedTags,
+//   };
+// }
+
+// function addFiles(tree: Tree, options: NormalizedSchema) {
+//     const templateOptions = {
+//       ...options,
+//       ...names(options.name),
+//       offsetFromRoot: offsetFromRoot(options.projectRoot),
+//       template: ''
+//     };
+//     generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
+// }
+
+// export default async function (tree: Tree, options: UtilLibGeneratorSchema) {
+//   // await libraryGenerator(tree, options);
+
+//   const normalizedOptions = normalizeOptions(tree, options);
+
+//   addProjectConfiguration(
+//     tree,
+//     normalizedOptions.projectName,
+//     {
+//       root: normalizedOptions.projectRoot,
+//       projectType: 'library',
+//       sourceRoot: `${normalizedOptions.projectRoot}/src`,
+//       targets: {
+//         build: {
+//           executor: "@bg-hoard/internal-plugin:build",
+//         },
+//       },
+//       tags: normalizedOptions.parsedTags,
+//     }
+//   );
+//   addFiles(tree, normalizedOptions);
+//   await formatFiles(tree);
+// }
